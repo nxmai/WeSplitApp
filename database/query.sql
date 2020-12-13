86d87ec57522c898c959999233d67bbce8562a31
nxmai <nxmai.it@gmail.com>
2020-12-13 16:56:36 +0700

    add sql query and fake data

---
+++ database/query.sql
@@ -0,0 +1,147 @@
+﻿create database wesplit
+go
+
+use wesplit
+go
+
+--bảng địa danh
+create table places
+(
+	id int not null,
+	name nvarchar(50),
+	address nvarchar(500),
+	discription nvarchar(1000)
+
+	constraint pk_places primary key (id)
+)
+go
+
+--bảng chuyến đi, 1 khóa ngoại tới địa danh
+create table trip
+(
+	id int not null,
+	name nvarchar(50),
+	idplace int,
+	thumbnail char(50),
+	datetogo date,
+	returndate date,
+	isfinish bit,
+	totalrevenue int,
+	totalexpend int
+
+	constraint pk_trip primary key (id)
+)
+go
+
+alter table trip add constraint fk_trip foreign key (idplace) references places(id)
+go
+
+--bảng hình ảnh, 1 chuyến đi có nhiều hình ảnh, 1 khóa ngoại tới chuyến đi, quan hệ một - nhiều
+create table image
+(
+	id int not null,
+	path char(50),
+	idtrip int
+
+	constraint pk_image primary key (id, idtrip)
+)
+go
+
+alter table image add constraint fk_image foreign key (idtrip) references trip(id)
+go
+
+--bảng lộ trình, 1 chuyến đi có nhiều lộ trình, 1 khóa ngoại tới chuyến đi, quan hệ một-nhiều
+create table route
+(
+	id int not null,
+	place nvarchar(50),
+	cost int,
+	idtrip int
+
+	constraint pk_route primary key (id, idtrip)
+)
+
+alter table route add constraint fk_route foreign key (idtrip) references trip(id)
+go
+
+--bảng thành viên
+create table member 
+(
+	id int not null,
+	name nvarchar(50),
+	phonenumber char(11),
+	collectedmoney int,
+	idtrip int
+
+	constraint pk_member primary key (id, idtrip)
+)
+go
+
+alter table member add constraint fk_member foreign key (idtrip) references trip(id)
+go
+
+
+insert into places values ('1', N'Biển Vũng Tàu', N'Bãi sau Vũng Tàu, thành phố Vũng Tàu, tỉnh Bà Rịa - Vũng Tàu', 'Vũng Tàu là một thành phố thuộc tỉnh Bà Rịa - Vũng Tàu, ở vùng Đông Nam Bộ, Việt Nam. Đây là trung tâm kinh tế, tài chính, văn hóa, du lịch, giao thông - vận tải và giáo dục và là một trong những trung tâm kinh tế của vùng Đông Nam Bộ');
+insert into places values ('2', N'Đà Lạt', N'Tỉnh Lâm Đồng', N'Đà Lạt là thành phố tỉnh lỵ của tỉnh Lâm Đồng, nằm trên cao nguyên Lâm Viên, thuộc vùng Tây Nguyên, Việt Nam. Từ xa xưa, vùng đất này vốn là địa bàn cư trú của những cư dân người Lạch, người Chil và người Srê thuộc dân tộc Cơ Ho.');
+insert into places values ('3', N'Đà Nẵng', N'Tỉnh Đà Nẵng', N'Đà Nẵng là một thành phố trực thuộc trung ương, nằm trong vùng Duyên hải Nam Trung Bộ Việt Nam, là thành phố trung tâm và lớn nhất khu vực miền Trung - Tây Nguyên.');
+
+
+insert into trip values ('1', N'Tắm biển mùa hè', '1', 'Data/fakedata/vungtau_thumbnail.jpg', '1/6/2020', '3/6/2020', 1, null, null);
+--insert into trip values ('2', 'Tắm biển mùa đông', '1', 'Data/fakedata/vungtau_thumbnail.jpg', '5/7/2020', '13/7/2020', 0, null, null);
+insert into trip values ('3', N'Du lịch Đà Lạt', '2', 'Data/fakedata/dalat_thumbnail.jpg', '5/7/2020', '12/7/2020', 1, null, null);
+--insert into trip values ('4', N'Đi Đà Nẵng', '3', 'Data/fakedata/danang_thumbnail.jpg', '5/2/2020', '13/2/2020', 1, null, null);
+
+
+insert into image values ('1', 'Data/fakedata/vungtau1.jpg', '1');
+insert into image values ('2', 'Data/fakedata/vungtau2.jpg', '1');
+insert into image values ('3', 'Data/fakedata/vungtau3.jpg', '1');
+
+--insert into image values ('4', 'Data/fakedata/vungtau1.jpg', '2');
+--insert into image values ('5', 'Data/fakedata/vungtau2.jpg', '2');
+--insert into image values ('6', 'Data/fakedata/vungtau3.jpg', '2');
+
+insert into image values ('7', 'Data/fakedata/dalat1.jpg', '3');
+insert into image values ('8', 'Data/fakedata/dalat2.jpg', '3');
+insert into image values ('9', 'Data/fakedata/dalat3.jpg', '3');
+
+--insert into image values ('10', 'Data/fakedata/danang1.jpg', '4');
+--insert into image values ('11', 'Data/fakedata/danang2.jpg', '4');
+--insert into image values ('12', 'Data/fakedata/danang3.jpg', '4');
+
+
+insert into route values ('1', N'Đại học Khoa học tự nhiên', 50000, '1');
+insert into route values ('2', N'Làng đại học', 70000, '1');
+insert into route values ('3', N'Bò sữa Long Thành', 100000, '1');
+insert into route values ('4', N'Bãi sau Vũng Tàu', 20000, '1');
+
+--insert into route values ('5', 'Đại học Khoa học tự nhiên 2', 60000, '2');
+--insert into route values ('6', 'Làng đại học 2', 80000, '2');
+--insert into route values ('7', 'Bò sữa Long Thành 2', 110000, '2');
+--insert into route values ('8', 'Bãi sau Vũng Tàu 2', 21000, '2');
+
+insert into route values ('9', N'Đại học Khoa học tự nhiên 3 ', 50000, '3');
+insert into route values ('10', N'Làng đại học 3', 70000, '3');
+insert into route values ('11', N'Cao nguyên Lâm Viên ', 100000, '3');
+insert into route values ('12', N'Thành phố Đà Lạt', 20000, '3');
+
+--insert into route values ('13', N'Đại học Khoa học tự nhiên 4', 150000, '4');
+--insert into route values ('14', N'Làng đại học 4', 710000, '4');
+--insert into route values ('15', N'Đèo Hải Vân', 1100000, '4');
+--insert into route values ('16', N'Đà Nẵng', 120000, '4');
+
+
+insert into member values ('1', N'Nguyễn Xuân Mai', '012345678', 120000, '1');
+insert into member values ('2', N'Khưu Thùy Kỳ', '152637934', 240000, '1');
+insert into member values ('3', N'Dương Bội Long', '019826421', 300000, '1');
+
+--insert into member values ('4', 'Nguyễn Xuân Mai 2', '012345678', 220000, '2');
+--insert into member values ('5', 'Khưu Thùy Kỳ 2', '152637934', 340000, '2');
+--insert into member values ('6', 'Dương Bội Long 2', '019826421', 400000, '2');
+
+insert into member values ('7', N'Nguyễn Xuân Mai 3', '012345678', 1200000, '3');
+insert into member values ('8', N'Khưu Thùy Kỳ 3', '152637934', 2400000, '3');
+insert into member values ('9', N'Dương Bội Long 3', '019826421', 3000000, '3');
+
+--insert into member values ('10', N'Nguyễn Xuân Mai 4', '012345678', 200000, '4');
+--insert into member values ('11', N'Khưu Thùy Kỳ 4', '152637934', 240000, '4');
+--insert into member values ('12', N'Dương Bội Long 4', '019826421', 300000, '4');
